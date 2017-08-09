96c6bdf7640ad7f31706fa2abec71c6c556bfa88
kaich <chengkai1853@163.com>
2017-08-09 15:33:42 +0800

    fix bug

--- lib/zadtool.rb
+++ lib/zadtool.rb
@@ -1,5 +1,6 @@
 require "zadtool/version"
 require 'optparse'
+require 'open-uri'
 
 module Zadtool
 
@@ -9,32 +10,32 @@ module Zadtool
     OptionParser.new do |opts|
       opts.banner = "Usage: zad tool for zad project"
 
-      opts.on("-o", "--obscure XcodeprojPath", "obscure code") do |v|
-        options[:obscure] = v
+      opts.on("-s", "--string urlstring", "url encode for urlString") do |v|
+        options[:str] = v
       end
 
-      opts.on("-l", "--load path1,path2,path3",Array,"load filt symbols from path") do |v|
-        options[:load] = v 
-      end
-
-      opts.on("-r", "--reset", "reset loaded symbols") do |v|
-        options[:reset] = true
-      end
-
-      opts.on("-f", "--fetch type1,type2,type3", "fetch and replace type,default type is [c,p,f].c for class,p for property,f for function") do |v|
-        options[:fetch] = v
-      end
-
-      opts.on("-i", "--ignore XcodeprojPath", "create a ignore file, you can write your filt symbols in it.eg:name,age ...") do |v|
-        options[:ignore] = v 
-      end
-
-      opts.on("-t", "--type replaceType", "obscure type = [r,w,c] ,r: random w: random words c: custom replace rule") do |v|
-        options[:type] = v 
+      opts.on("-f", "--file path","url encode for content of path") do |v|
+        options[:file] = v
       end
 
     end.parse!
 
+    str = options[:str]
+    if str
+        puts "---------------------"
+        puts URI::encode(str)
+    end
+
+    path = options[:file]
+    if path
+        results = ""
+        File.readlines(path).each do |line|
+            encodeStr = URI::encode line
+            results = results + encodeStr + "\n"
+        end
+        puts "---------------------"
+        puts results
+    end
     
   end
 
