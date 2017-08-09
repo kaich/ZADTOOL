257abf20c1af4103edb282abb91947e43396f17e
kaich <chengkai1853@163.com>
2017-08-09 15:39:05 +0800

    baseurl

--- lib/zadtool.rb
+++ lib/zadtool.rb
@@ -18,12 +18,21 @@ module Zadtool
         options[:file] = v
       end
 
+      opts.on("-b", "--base baseURL","base url for url encode") do |v|
+        options[:base] = v
+      end
+
     end.parse!
+
+    base_url = options[:base]
+    if base_url == nil
+        base_url = ""
+    end
 
     str = options[:str]
     if str
         puts "---------------------"
-        puts URI::encode(str)
+        puts(base_url + URI::encode(str))
     end
 
     path = options[:file]
@@ -34,7 +43,7 @@ module Zadtool
             results = results + encodeStr + "\n"
         end
         puts "---------------------"
-        puts results
+        puts(base_url + results)
     end
     
   end
